2160df34701ea36707280f66c4ee21bbcfc05000
Beatris Borislavova <betivd03@abv.bg>
2021-12-13 18:51:12 +0200

    Add getAll function to the Gallery

--- src/services/photosService.js
+++ src/services/photosService.js
@@ -1,9 +1,8 @@
-import * as api from "../helpers/api.js";
-import * as request from "../helpers/requester.js";
+const baseUrl = "http://localhost:3030/data";
 
-export const getAll = () => request.get(`${api.photos}?sortBy=_createdOn%20desc`);
-export const getMyPhotos = (id) => request.get(`${api.photos}?where=_ownerId%3D%22${id}%22&sortBy=_createdOn%20desc`);
-export const getPhoto = (id) => request.get(`${api.photos}/${id}`);
-export const addPhoto = (photo) => request.post(api.photos, photo);
-export const editPhoto = (id, photo) => request.put(`${api.photos}/${id}`, photo);
-export const deletePhoto = (id) => request.del(`${api.photos}/${id}`);
+export const getAll = async () => {
+    let response = await fetch(`${baseUrl}/photos`);
+    let photos = await response.json();
+    let result = Object.values(photos);
+    return result;
+};
