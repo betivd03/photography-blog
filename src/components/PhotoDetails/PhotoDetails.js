c45d8eab46eed0087303c6f6a8b8f2ac1d9f56b0
Beatris Borislavova <betivd03@abv.bg>
2021-12-14 20:01:45 +0200

    Add owner and separation of ownerButtons and userButtons in PhotoDetails

--- src/components/PhotoDetails/PhotoDetails.js
+++ src/components/PhotoDetails/PhotoDetails.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useContext } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext.js";
 
 import * as photosService from "../../services/photosService.js";
 
@@ -9,14 +9,30 @@ const PhotoDetails = () => {
 
     const [details, setDetails] = useState([]);
 
+    const { user } = useContext(AuthContext);
+
     const { photoId } = useParams();
 
     useEffect(() => {
         photosService.getOne(photoId)
             .then(result => {
-                setDetails(result)
-            })
-    }, []);
+                setDetails(result);
+            });
+        }, []);
+
+    let ownerButtons = (
+        <div className="owner">
+            <Link to="/details/photoId">Edit</Link>
+            <Link to="/details/photoId">Delete</Link>
+        </div>
+    );
+
+    let userButtons = (
+        <div className="user">
+            <Link to="/details/photoId">Like</Link>
+            <span>Liked</span>
+        </div>
+    );
 
     return (
         <section id="photo-details-page">
@@ -28,17 +44,21 @@ const PhotoDetails = () => {
                         <li>Category: <span>{details.category}</span></li>
                         <li>Description: <span>{details.description}</span></li>
                         <li>Location: <span>{details.location}</span></li>
-                        <li>Created By: <span>@username</span></li>
+                        <li>Created By: <span>{details.owner}</span></li>
                     </ul>
-                    <div className="owner">
-                        <Link to="/details/photoId">Edit</Link>
-                        <Link to="/details/photoId">Delete</Link>
+
+                    <div className="buttons">
+                        {user._id && (user._id === details._ownerId
+                            ? ownerButtons
+                            : userButtons
+                        )}
+
+                        <div className="likes">
+                            <span>Liked by: 3 people</span>
+                        </div>
                     </div>
-                    <div className="user">
-                        <Link to="/details/photoId">Like</Link>
-                        <span>Liked</span>
-                        <span>Liked by: 3 people</span>
-                    </div>
+                    
+                    
                 </div>
                 <div className="photo-image">
                     <img src={details.imageUrl} alt={details.title} />
