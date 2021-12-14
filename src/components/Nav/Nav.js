7fd3428d3c9b249adb30b888991d265e8d89d5cf
Beatris Borislavova <betivd03@abv.bg>
2021-12-14 13:12:18 +0200

    Separation of userNav and guestNav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext.js";
+
 const Nav = () => {
+    const { user } = useContext(AuthContext);
+
+    let userNav = (
+        <div className="logged">
+            <a href="/add">Add Photo</a>
+            <a href="/my-profile">My Profile</a>
+            <a href="/logout">Logout</a>
+            <p id="welcome">Welcome, {user.username}</p>
+        </div>
+    );
+
+    let guestNav = (
+        <div className="guest">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        </div>
+    );
+
     return (
         <nav>
             <div className="nav-image">
@@ -7,16 +28,11 @@ const Nav = () => {
             <div className="navbar">
                 <a href="/gallery">Gallery</a>
                 <a href="/about">About</a>
-                <div className="logged">
-                    <a href="/add">Add Photo</a>
-                    <a href="/my-profile">My Profile</a>
-                    <a href="/logout">Logout</a>
-                    <p id="welcome">Welcome, @username</p>
-                </div>
-                <div className="guest">
-                    <a href="/login">Login</a>
-                    <a href="/register">Register</a>
-                </div>
+                
+                {user.username 
+                    ? userNav
+                    : guestNav}
+                
             </div>
         </nav>
     );
